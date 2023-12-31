0ce3cd91daec5fc5969a2745dddd79b47e74d4e5
pranjja1 <138505130+pranjja1@users.noreply.github.com>
2023-12-31 13:35:03 +0530

    Create pepperoni-pizza-removebg-previ.js

---
+++ pepperoni-pizza-removebg-previ.js
@@ -0,0 +1,8 @@
+import { memo } from "react";
+import "./pepperoni-pizza-removebg-previ.css";
+
+const PepperoniPizzaRemovebgPrevi = memo(() => {
+  return <div className="pepperoni-pizza-removebg-previ" />;
+});
+
+export default PepperoniPizzaRemovebgPrevi;
