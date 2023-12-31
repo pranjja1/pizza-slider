2a8eaa7c48a72c18b34c81933cc63b6603dbd745
pranjja1 <138505130+pranjja1@users.noreply.github.com>
2023-12-31 13:34:13 +0530

    Create th-removebg-preview3.js

---
+++ th-removebg-preview3.js
@@ -0,0 +1,8 @@
+import { memo } from "react";
+import "./th-removebg-preview3.css";
+
+const ThRemovebgPreview3 = memo(() => {
+  return <div className="th-removebg-preview-3" />;
+});
+
+export default ThRemovebgPreview3;
