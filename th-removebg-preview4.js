d1489a62192feec247f8192b866c0b5f71c461ec
pranjja1 <138505130+pranjja1@users.noreply.github.com>
2023-12-31 13:33:21 +0530

    Create th-removebg-preview4.js

---
+++ th-removebg-preview4.js
@@ -0,0 +1,8 @@
+import { memo } from "react";
+import "./th-removebg-preview4.css";
+
+const ThRemovebgPreview4 = memo(() => {
+  return <div className="th-removebg-preview-4" />;
+});
+
+export default ThRemovebgPreview4;
